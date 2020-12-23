713ed6bdf5ac567c948610e50eba20453772f46c
koko2824 <prg.koko2824@gmail.com>
2020-12-23 23:10:02 +0900

    system implementation

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"birthday_bot/slack"
 	"birthday_bot/spreadsheet"
 	"fmt"
 	"github.com/joho/godotenv"
+	"time"
 )
 
 func main() {
@@ -13,11 +15,36 @@ func main() {
 		return
 	}
 
-	sheetData, err:= spreadsheet.GetData()
+	loc, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	nowTime := time.Now().In(loc)
+	nowTime = time.Date(2020, 6, 10, 0, 0, 0, 0, loc)
+	_, m, d := nowTime.Date()
+	sheetData, err := spreadsheet.GetData()
 
-	fmt.Println(sheetData)
+	var message string
+	for _, v := range sheetData {
+		fmt.Println(v.Birthday)
+		t, err := time.Parse("2006-01-02 15:04:05 -0700", v.Birthday)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if t.Month() == m && t.Day() == d {
+			message += fmt.Sprintf("<@%s>さん", v.Slack)
+		}
+	}
+
+	if message != "" {
+		res := fmt.Sprintf("今日は%sの誕生日です！\nみんなでお祝いしましょう！", message)
+		fmt.Println(res)
+		slack.SendMessage(res)
+		return
+	}
+
+	fmt.Println("今日誕生日の人はいません。")
 }
